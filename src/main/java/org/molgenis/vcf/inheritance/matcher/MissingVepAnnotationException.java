419f9723aa8927e95825d3954159a915d195e6cd
Dennis Hendriksen <d.hendriksen@umcg.nl>
2023-08-29 13:07:35 +0200

    Bump dependencies, fix warnings, remove dead code (#36)
    * chore:bump version 2.1.6 to 2.1.7
    chore:bump spring-boot-starter-parent 3.0.5 to 3.1.3
    chore:bump jacoco-maven-plugin 0.8.9 to 0.8.10
    chore:bump vip-utils 1.2.1 to 1.3.0
    
    * chore:remove dead code
    
    * fix:serialization warnings

--- src/main/java/org/molgenis/vcf/inheritance/matcher/MissingVepAnnotationException.java
+++ src/main/java/org/molgenis/vcf/inheritance/matcher/MissingVepAnnotationException.java
@@ -1,9 +1,13 @@
 package org.molgenis.vcf.inheritance.matcher;
 
+import java.io.Serial;
+
 import static java.lang.String.format;
 
 public class MissingVepAnnotationException extends
     RuntimeException {
+  @Serial
+  private static final long serialVersionUID = 1L;
 
   public MissingVepAnnotationException(String field) {
     super(format("VEP annotation is missing field '%s'.", field));
