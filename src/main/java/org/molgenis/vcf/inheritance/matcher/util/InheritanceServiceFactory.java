021238054db03bd35ea7b0e7f451b2c2351bb17b
Dennis Hendriksen <d.hendriksen@umcg.nl>
2024-01-26 10:53:18 +0100

    Remove dependency on field_metadata.json (#49)
    * chore:remove dependency on field_metadata.json
    
    * chore:bump vip-utils 1.4.5 to 1.4.6, bump version 3.0.1 to 3.0.2
    
    * fix VepMetadataServiceFactoryImpl

---
+++ src/main/java/org/molgenis/vcf/inheritance/matcher/util/InheritanceServiceFactory.java
@@ -0,0 +1,7 @@
+package org.molgenis.vcf.inheritance.matcher.util;
+
+import org.molgenis.vcf.inheritance.matcher.InheritanceService;
+
+public interface InheritanceServiceFactory {
+    InheritanceService create();
+}
